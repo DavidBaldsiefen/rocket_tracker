20232933d114018619ee82323f0a9c06bf98773f
DavidBaldsiefen <david.baldsiefen@gmx.de>
2021-11-28 10:20:36 +0700

    Add basic framegrabber using image_transport and OpenCV4

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Rocket-Tracker
+
+## Requirements
+
+- OpenCV 4
+- ROS melodic
+
+In order to successfully compile and use this package, a vision_opencv package that was compiled against OpenCV 4 is required. For example, you can clone [this](https://github.com/DavidBaldsiefen/vision_opencv) into your `catkin_ws/src` folder.
--- core/src/frame_grabber_node.cpp
+++ core/src/frame_grabber_node.cpp
@@ -1,3 +1,64 @@
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <opencv4/opencv2/opencv.hpp>
+#include <ros/ros.h>
+
+cv::VideoCapture capture;
+
+bool init() {
+    // Set Log-level
+    if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) {
+        ros::console::notifyLoggerLevelsChanged();
+    }
+
+    // Open video file
+    capture = cv::VideoCapture("/home/david/Downloads/silent_launches.mp4");
+    if (!capture.isOpened()) {
+        capture.release();
+        ROS_ERROR("Failed to open capture!");
+        return false;
+    }
+
+    return true;
+}
+
 int main(int argc, char **argv) {
+    if (!init()) {
+        return 0;
+    }
+
+    ros::init(argc, argv, "FRAMEGRABBER");
+    ros::NodeHandle nh("~");
+
+    // Creating image-transport publisher for rqt
+    image_transport::ImageTransport it(nh);
+    // queuesize = fps * 2, so there is a 2 seconds buffer to publish
+    image_transport::Publisher pubimg =
+        it.advertise("/image_topic", (uint32_t)capture.get(cv::CAP_PROP_FPS) * 2);
+
+    sensor_msgs::ImagePtr msg;
+
+    cv::Mat videoFrame;
+
+    // Main loop
+    ros::Rate r(capture.get(cv::CAP_PROP_FPS)); // Set loop rate to video fps
+    while (ros::ok()) {
+
+        if (!capture.read(videoFrame)) {
+            ROS_INFO("End of video reached - shutdown.");
+            break;
+        }
+
+        msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", videoFrame).toImageMsg();
+        pubimg.publish(msg);
+
+        ros::spinOnce();
+        r.sleep();
+    }
+
+    // Shut everything down cleanly
+    ros::shutdown();
+    pubimg.shutdown();
+    nh.shutdown();
     return 0;
 }
